fbdcb80cff103ab590e34156c8c57579493e231a
mantasdev <mantom94@gmail.com>
2016-12-20 15:03:18 +0200

    service worker, app manifest

---
+++ sw.js
@@ -0,0 +1,37 @@
+// Use a cacheName for cache versioning
+var cacheName = 'v1:static';
+
+// During the installation phase, you'll usually want to cache static assets.
+self.addEventListener('install', function(e) {
+    // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './style.css',
+                './mainController.js',
+                './busData.json',
+                './app.js',
+                'https://fonts.googleapis.com/css?family=Roboto:400,500,700,300',
+                'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js'
+            ]).then(function() {
+                self.skipWaiting();
+            });
+        })
+    );
+});
+
+// when the browser fetches a URL…
+self.addEventListener('fetch', function(event) {
+    // … either respond with the cached object or go ahead and fetch the actual URL
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
+});
